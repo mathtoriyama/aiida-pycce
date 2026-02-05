f05d35360c8d7485f6489572745f4c0cc57fdad7
mathtoriyama <mathtoriyama@gmail.com>
2026-02-05 16:17:59 -0600

    Tutorial

--- examples/HT_2D_Hosts.py
+++ examples/HT_2D_Hosts.py
@@ -1,30 +1,35 @@
 
+# Import AiiDA-related packages
 import aiida
 from aiida.engine import submit
 from aiida.orm import Str, StructureData, load_code
 
+# Import helper packages
 import numpy as np
 import os, sys
 from ase.io import read
 from glob import iglob
 from copy import deepcopy
 
+# Import PyCCE work chain, to be submitted as jobs
 from Chain_PyCCE import Chain_PyCCE
 
+### -----------------------------------------------------
+### This example submission script assumes that the AiiDA
+### framework has been set up properly on your machine.
+### 
+### AiiDA can be installed by following the instructions here: 
+###   https://aiida.readthedocs.io/projects/aiida-core/en/stable/installation/index.html
+### -----------------------------------------------------
+
+# Load profile
 aiida.load_profile()
+
+# Load code (aiida-pycce)
 code_pycce = load_code(label="pycce2d@midway")
 
-parameters_pycce = {        
-    "r_bath": 120,
-    "r_dipole": 30,
-    "order": 2,
-    "mag_field": 50000,
-    "pulses": 1,
-    "mintime": 0,
-    "maxtime": 50,
-    "time_npoints": 1001,
-}
-
+# Define job submission details
+# See CalcJob documentation for more details (https://aiida.readthedocs.io/projects/aiida-core/en/stable/topics/calculations/usage.html#options)
 custom_metadata = {
     "options": {
         "account": "pi-gagalli",
@@ -39,17 +44,27 @@ custom_metadata = {
     }
 }
 
+# Define basic input parameters
+parameters_pycce = {        
+    "r_bath": 120,
+    "r_dipole": 30,
+    "order": 2,
+    "mag_field": 50000,
+    "pulses": 1,
+    "mintime": 0,
+    "maxtime": 50,
+    "time_npoints": 1001,
+}
 
+# Gather crystal structures
 structures = {}
 for ciffile in iglob("Structures/*.cif"):
-    structure = StructureData(ase=read(ciffile))
+    structure = StructureData(ase=read(ciffile))  ## Important: Ensure that the structure data type is the AiiDA StructureData object
     name = ciffile.split("/")[-1].split(".cif")[0]
-    print(name)
     structures[name] = structure
-print(structures)
 
 
-# Submit calculations
+# Submit calculations (1 job per crystal structure)
 for cif_code in structures.keys():
 
     # Gather inputs
@@ -64,5 +79,5 @@ for cif_code in structures.keys():
     # Submit WorkChain
     chain = submit(Chain_PyCCE, **inputs)
 
-    print(f"Submitted {chain}")
+    print(f"Submitted: {chain}")
 
